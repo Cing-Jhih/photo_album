3db773a18770ebd95fc04fb9cc8ab413a3324bb8
Allie <zxp800310@gmail.com>
2018-06-20 03:21:00 +0800

    add edit and update action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,6 +19,10 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
 private
 
   def photo_params
