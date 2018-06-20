36d3f85f7741f4fc231a3684d780d2494fb89bb6
Allie <zxp800310@gmail.com>
2018-06-21 02:30:55 +0800

    add before_action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+before_action :set_photo, :only =>[:show, :edit, :update, :destroy]
 
   def index
     @photos = Photo.all
@@ -18,15 +19,12 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @photo = Photo.find(params[:id])
   end
 
   def edit
-    @photo = Photo.find(params[:id])
   end
 
   def update
-    @photo = Photo.find(params[:id])
     if @photo.update_attributes(photo_params)
       redirect_to photo_path(@photo)
     else
@@ -35,13 +33,15 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
     @photo.destroy
-
     redirect_to photos_path
   end
 
 private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
   def photo_params
     params.require(:photo).permit(:title, :date, :description, :file_location)
